5e5d973af018e8708619f51b45fcc480f1cd5f3e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-10-26 13:03:42 +0300

    Update

---
+++ ReactJS/Intro to React and JSX/react-demo-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Spinner from "./components/Spinner/Spinner";
+import Topbar from "./components/Topbar/Topbar";
+
+function App() {
+    return (
+        <>
+            {/* <Spinner /> */}
+            <Topbar />
+            <Navbar />
+        </>
+    );
+}
+
+export default App;
