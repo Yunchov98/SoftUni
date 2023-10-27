20911e0c6d6ba9b1e728c9d05d6464dbbad4f41a
Georgi Yunchov <georgiiunchov@gmail.com>
2023-10-27 17:29:55 +0300

    Add footer component

---
+++ ReactJS/Components Basic Idea/components-basics/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles["footer-container"]}>
+            <p>All rights reserved &copy; GY</p>
+        </footer>
+    );
+}
