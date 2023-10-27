3e831b34a9d50d087f68cdf73fea906629382ac6
Georgi Yunchov <georgiiunchov@gmail.com>
2023-10-27 17:30:41 +0300

    Add header component

---
+++ ReactJS/Components Basic Idea/components-basics/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles["header-container"]}>
+            <h1>Components Basic Idea</h1>
+        </header>
+    );
+}
