5b724142fa3ffb496a935e9c1999f9513c33136f
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-02 17:31:02 +0200

    Add todo-list component

--- ReactJS/Components Deep Dive/todo-list/client/src/App.jsx
+++ ReactJS/Components Deep Dive/todo-list/client/src/App.jsx
@@ -1,10 +1,15 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
     return (
         <>
             <Header />
+
+            <main className="main">
+                <TodoList />
+            </main>
 
             <Footer />
         </>
