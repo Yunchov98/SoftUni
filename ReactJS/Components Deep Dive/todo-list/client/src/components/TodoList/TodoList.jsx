2e84f02cef5968f1c97597214ad1d327e20002ee
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-02 17:51:20 +0200

    Add spinner

--- ReactJS/Components Deep Dive/todo-list/client/src/components/TodoList/TodoList.jsx
+++ ReactJS/Components Deep Dive/todo-list/client/src/components/TodoList/TodoList.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import TodoItem from './TodoItem/TodoItem';
+import Spinner from '../Spinner/Spinner';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:3030/jsonstore/todos')
             .then((response) => response.json())
             .then((data) => {
                 setTodos(Object.values(data));
+                setIsLoading(false);
             })
             .catch((err) => console.log(err));
     }, []);
@@ -41,34 +44,32 @@ export default function TodoList() {
                 </button>
             </div>
 
-            <div className="table-wrapper">
-                {/* <div className="loading-container">
-                    <div className="loading-spinner">
-                        <span className="loading-spinner-text">Loading</span>
-                    </div>
-                </div> */}
-
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th className="table-header-task">Task</th>
-                            <th className="table-header-status">Status</th>
-                            <th className="table-header-action">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {todos.map((todo) => (
-                            <TodoItem
-                                key={todo._id}
-                                _id={todo._id}
-                                text={todo.text}
-                                isCompleted={todo.isCompleted}
-                                changeStatusHandler={changeStatusHandler}
-                            />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <div className="table-wrapper">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th className="table-header-task">Task</th>
+                                <th className="table-header-status">Status</th>
+                                <th className="table-header-action">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {todos.map((todo) => (
+                                <TodoItem
+                                    key={todo._id}
+                                    _id={todo._id}
+                                    text={todo.text}
+                                    isCompleted={todo.isCompleted}
+                                    changeStatusHandler={changeStatusHandler}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </section>
     );
 }
