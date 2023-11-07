17f22d35d899498c8497a7ad599d1401fea5e1ab
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-07 14:50:02 +0200

    Add footer component

---
+++ ReactJS/Workshop Components/client/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return (
+        <footer class="footer">
+            <p>
+                This site is designed to be used for training purposes at
+                SoftUni.
+            </p>
+        </footer>
+    );
+};
