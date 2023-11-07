5f23b418f00f850adc120dad12fbb64a21a99817
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-07 15:09:33 +0200

    Add button for people adding

--- ReactJS/Workshop Components/client/src/components/UserList/UserList.jsx
+++ ReactJS/Workshop Components/client/src/components/UserList/UserList.jsx
@@ -1,11 +1,14 @@
-import { Search } from "../Search/Search";
-import { UserListTable } from "./UserListTable/UserListTable";
+import { Search } from '../Search/Search';
+import { UserListTable } from './UserListTable/UserListTable';
 
 export const UserList = () => {
     return (
         <section className="card users-container">
             <Search />
+
             <UserListTable />
+
+            <button className="btn-add btn">Add new user</button>
         </section>
     );
-}
+};
