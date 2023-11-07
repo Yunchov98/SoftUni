681c110ea4e022b4e6ef071d0b7130d6cd7abdab
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-07 15:00:31 +0200

    Add user-list component

---
+++ ReactJS/Workshop Components/client/src/components/UserList/UserList.jsx
@@ -0,0 +1,9 @@
+import { Search } from "../Search/Search";
+
+export const UserList = () => {
+    return (
+        <section className="card users-container">
+            <Search />
+        </section>
+    );
+}
