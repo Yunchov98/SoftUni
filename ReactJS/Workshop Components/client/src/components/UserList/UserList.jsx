9594e96451112d976b07922a98b1716a5b7ca4e4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-07 15:03:53 +0200

    Add user-list table

--- ReactJS/Workshop Components/client/src/components/UserList/UserList.jsx
+++ ReactJS/Workshop Components/client/src/components/UserList/UserList.jsx
@@ -1,9 +1,11 @@
 import { Search } from "../Search/Search";
+import { UserListTable } from "./UserListTable/UserListTable";
 
 export const UserList = () => {
     return (
         <section className="card users-container">
             <Search />
+            <UserListTable />
         </section>
     );
 }
